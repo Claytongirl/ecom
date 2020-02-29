5a25a1eae2551a2ec738b6cb8bf09d351ec95fdf
Claytongirl <lisadeclayton@gmail.com>
2020-02-29 13:12:01 -0700

    Structured Shipping form js and scss

--- src/components/information/payment.js
+++ src/components/information/payment.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import PageTitle from "../pageTitle";
+import PaymentForm from './paymentForm';
 
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
@@ -18,7 +19,7 @@ class Payment extends Component {
         return (
             <div className = 'Payment'>
              <PageTitle className = "sign-in__page-title" title = "Payment Information"/>
-             {/*<PaymentForm onSubmit={this.onSubmit} className="sign-in__form">*/}
+             <PaymentForm onSubmit={this.onSubmit} className="sign-in__form"/>
             </div>
         )
     }
